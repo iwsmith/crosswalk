ab85cd76b5995bef13bc0c31ff578e7762a262be
Ian Wesley-Smith <iwsmith@gmail.com>
2018-05-20 01:56:25 +0000

    Support for square matrix

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 import subprocess
 import copy
+import os
 from flask import Flask, abort, request
 app = Flask(__name__)
 
 current_process = None
 
-DEMO_ARGS = ['demo', '--led-cols=64', '--led-chain=2', '-D']
+DEMO_ARGS = ['demo', '--led-cols=64', '--led-chain=2', '-L', '-D']
+IMG_VIEWER_ARGS = ['led-image-viewer', '--led-cols=64', '--led-no-hardware-pulse', '--led-chain=2','--led-gpio-mapping=adafruit-hat']
+
+IMAGES_PATH = os.getenv('CROSSWALK_IMAGES')
+
 
 @app.route("/kill")
 def kill_current():
@@ -26,7 +31,7 @@ def run_demo(n):
 
     args.append(str(n))
     if n is 1 or n is 2:
-        args.append(request.args["f"])
+        args.append(os.path.join(IMAGES_PATH, request.args["f"]))
         
     kill_current()
     current_process = subprocess.Popen(args)
