5864af15307fecf317189e8557c799765b0c8ca5
Ian Wesley-Smith <iwsmith@gmail.com>
2018-06-28 12:48:05 -0700

    Work those buttons

---
+++ button.py
@@ -0,0 +1,13 @@
+from gpiozero import Button
+from signal import pause
+import requests
+
+def button_press():
+    print("button")
+    requests.get("localhost/random")
+
+button = Button(18)
+
+button.when_released = button_press
+
+pause()
