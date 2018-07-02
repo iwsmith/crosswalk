aa93d005f30326ff8d75b77da5618867fac9c441
Greg Look <greg@greg-look.net>
2018-07-01 17:53:49 -0700

    WIP on xwalk demo and walk scenes.

--- xwalk/demo.py
+++ xwalk/demo.py
@@ -1,7 +1,12 @@
-import copy
+# Demo
 
-
-DEMO_ARGS = ['demo', '--led-cols=64', '--led-chain=2', '-L', '-D']
+DEMO_COMMAND = [
+    'demo',
+    '--led-cols=64',
+    '--led-chain=2',
+    '-L',
+    '-D',
+]
 
 
 class LEDDemo:
@@ -17,7 +22,8 @@ class LEDDemo:
 
     def command(self, image=None):
         """Return the command line args for rendering this demo."""
-        args = copy.copy(DEMO_ARGS)
+        args = []
+        args.extend(DEMO_COMMAND)
         args.append(str(self.demo_id))
 
         if self.ppm_required:
@@ -27,31 +33,21 @@ class LEDDemo:
 
         return args
 
-   # @staticmethod
-   # def from_yaml(filename, image_path=""):
-   #     """Load a collection of demos from a YAML file."""
-   #     with open(filename) as f:
-   #         demo_data = yaml.load(f)
-   #     desc = data['desc']
-   #     ppm = data['ppm'] in ['true', 'True']
-   #     image = data['image'] && os.path.join(image_path, data['image'])
-   #     return [LEDDemo(desc, ppm, image) for data in demo_data]
-
 
 DEMOS = [
     LEDDemo(i, desc, i in [1, 2])
     for i, desc in enumerate([
-            "Rotating Square",
-            "Forward Scrolling Image",
-            "Backward Scrolling Image",
-            "Test Color Square",
-            "Pulsing Color",
-            "Grayscale Block",
-            "Abelian Sandpile",
-            "Conway's Game of Life",
-            "Langton's Ant",
-            "Volume Bars",
-            "Evolution of Color",
-            "Brightness Pulse Generator",
-        ])
+        "Rotating Square",
+        "Forward Scrolling Image",
+        "Backward Scrolling Image",
+        "Test Color Square",
+        "Pulsing Color",
+        "Grayscale Block",
+        "Abelian Sandpile",
+        "Conway's Game of Life",
+        "Langton's Ant",
+        "Volume Bars",
+        "Evolution of Color",
+        "Brightness Pulse Generator",
+    ])
 ]
