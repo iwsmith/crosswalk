40ddea0c8b54c375681d37fe41d607eb8bf0fd1a
Greg Look <greg@greg-look.net>
2018-07-02 20:05:11 -0700

    Update demo controller code.

--- xwalk/demo.py
+++ xwalk/demo.py
@@ -1,4 +1,5 @@
-# Demo
+import subprocess
+
 
 DEMO_COMMAND = [
     'demo',
@@ -20,34 +21,62 @@ class LEDDemo:
         self.description = description
         self.ppm_required = ppm_required
 
-    def command(self, image=None):
-        """Return the command line args for rendering this demo."""
+
+class DemoController:
+    """
+    Controller to drive the LED panels with a demo program.
+    """
+
+    def __init__(self):
+        """Construct a new demo controller."""
+        self._process = None
+        self._playing = None
+        self.demos = {
+            i, LEDDemo(i, desc, i in [1, 2])
+            for i, desc in enumerate([
+                "Rotating Square",
+                "Forward Scrolling Image",
+                "Backward Scrolling Image",
+                "Test Color Square",
+                "Pulsing Color",
+                "Grayscale Block",
+                "Abelian Sandpile",
+                "Conway's Game of Life",
+                "Langton's Ant",
+                "Volume Bars",
+                "Evolution of Color",
+                "Brightness Pulse Generator",
+            ])
+        }
+
+
+    def play(demo_id, image=None):
+        """
+        Play the demo with the given identifier, if available in the
+        controller. Any currently playing demo will be replaced.
+        """
+        if demo_id not in self.demos:
+            raise ValueError("Demo id must be in {}".format(self.demos.keys()))
+        demo = self.demos[demo_id]
+
         args = []
         args.extend(DEMO_COMMAND)
-        args.append(str(self.demo_id))
+        args.append(str(demo.demo_id))
 
-        if self.ppm_required:
+        if demo.ppm_required:
             if image is None:
-                raise ValueError("A .ppm image must be provided")
-            args.append(self.image)
+                raise ValueError("A .ppm image must be provided for demo {}".format(demo.demo_id))
+            args.append(image)
 
-        return args
+        self.kill()
+        self._process = subprocess.Popen(args)
+        self._playing = demo_id
 
 
-DEMOS = [
-    LEDDemo(i, desc, i in [1, 2])
-    for i, desc in enumerate([
-        "Rotating Square",
-        "Forward Scrolling Image",
-        "Backward Scrolling Image",
-        "Test Color Square",
-        "Pulsing Color",
-        "Grayscale Block",
-        "Abelian Sandpile",
-        "Conway's Game of Life",
-        "Langton's Ant",
-        "Volume Bars",
-        "Evolution of Color",
-        "Brightness Pulse Generator",
-    ])
-]
+    def kill(self):
+        """Kill the currently running demo, if any."""
+        self._playing = None
+        if self._process:
+            #subprocess.call(['/usr/bin/pkill', '-P', str(self._process.pid)])
+            self._process.kill()
+            self._process = None
