b4332c9729e0d626ee3718c4582e04f5f33c1f48
Greg Look <greg@amperity.com>
2019-06-30 18:55:31 -0700

    Update provisioning and deploy logic.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ -z $1 ]]; then
-    echo "Usage: $(basename $0) <host>" >&2
+    echo "Usage: $(basename $0) <host> [--media]" >&2
     exit 1
 fi
 
@@ -15,10 +15,16 @@ ssh_sudo() {
 }
 
 echo "Copying code to ${DEPLOY_HOST}"
-rsync -rz --progress *.py config.yml static templates xwalk "${DEPLOY_USER}@${DEPLOY_HOST}:/tmp/crosswalk"
+rsync -rz --progress --delete xwalk templates config.yml run.py button.py "${DEPLOY_USER}@${DEPLOY_HOST}:/tmp/crosswalk"
+
+if [[ $1 = --media ]]; then
+    echo "Copying media to ${DEPLOY_HOST}"
+    rsync -rz --progress --delete static "${DEPLOY_USER}@${DEPLOY_HOST}:/tmp/crosswalk"
+fi
 
 echo "Synchronizing deployed code"
 ssh_sudo rsync -r /tmp/crosswalk/ /srv/crosswalk/code/
+ssh_sudo chown -R crosswalk:crosswalk /srv/crosswalk/code
 
 echo "Restarting crosswalk and button service"
 ssh_sudo systemctl restart button
