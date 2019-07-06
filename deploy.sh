cba0e7ceba85d6969b00ef6cfed80266c9d61488
Greg Look <greg@amperity.com>
2019-07-06 16:17:44 -0700

    Fancier deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,41 +3,58 @@
 set -e
 
 if [[ -z $1 ]]; then
-    echo "Usage: $(basename $0) <host> [--config|--media]" >&2
+    echo "Usage: $(basename $0) <host> [--config] [--code] [--media]" >&2
     exit 1
 fi
 
 DEPLOY_HOST="$1"
 DEPLOY_USER="${DEPLOY_USER:-pi}"
+DEPLOY_DIR=/srv/crosswalk/code
+
+DEPLOY_DEST="${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_DIR}"
 
 ssh_sudo() {
     ssh "${DEPLOY_USER}@${DEPLOY_HOST}" "sudo $@"
 }
 
+echo "Switching permissions during deploy..."
+ssh_sudo chown -R $DEPLOY_USER $DEPLOY_DIR
 
-if [[ $2 == --config ]]; then
-    echo "Copying config to ${DEPLOY_HOST}"
-    scp config.yml "${DEPLOY_USER}@${DEPLOY_HOST}:/tmp/crosswalk/config.yml"
+shift
+if [[ $# -eq 0 ]]; then
+    SYNC_CONFIG=1
+    SYNC_CODE=1
+    SYNC_MEDIA=1
 else
+    for opt in $@; do
+        case $opt in
+            --config) SYNC_CONFIG=1 ;;
+            --code)   SYNC_CODE=1   ;;
+            --media)  SYNC_MEDIA=1  ;;
+            *)
+                echo "Unknown option: $opt" >&2
+                exit 1
+        esac
+    done
+fi
+
+if [[ $SYNC_CONFIG ]]; then
+    echo "Copying config to ${DEPLOY_HOST}"
+    scp config.yml $DEPLOY_DEST/config.yml
+fi
+
+if [[ $SYNC_CODE ]]; then
     echo "Copying code to ${DEPLOY_HOST}"
-    rsync -rz --progress --delete xwalk templates config.yml run.py button.py "${DEPLOY_USER}@${DEPLOY_HOST}:/tmp/crosswalk"
+    rsync -rz --progress --delete xwalk templates config.yml run.py button.py $DEPLOY_DEST
 fi
 
-if [[ $2 == --media ]]; then
+if [[ $SYNC_MEDIA ]]; then
     echo "Copying media to ${DEPLOY_HOST}"
-    rsync -rz --progress --delete static "${DEPLOY_USER}@${DEPLOY_HOST}:/tmp/crosswalk"
+    rsync -rz --progress --delete static/ $DEPLOY_DEST/static/
 fi
 
-echo "Synchronizing deployed code"
-ssh_sudo rsync -r /tmp/crosswalk/xwalk /tmp/crosswalk/templates /tmp/crosswalk/config.yml /tmp/crosswalk/run.py /tmp/crosswalk/button.py /srv/crosswalk/code/
-
-if [[ $2 == --media ]]; then
-    echo "Synchronizing media"
-    ssh_sudo rsync -r /tmp/crosswalk/static/ /srv/crosswalk/code/static/
-fi
-
-echo "Fixing permissions"
-ssh_sudo chown -R crosswalk:crosswalk /srv/crosswalk/code
+echo "Resetting permissions..."
+ssh_sudo chown -R crosswalk:crosswalk $DEPLOY_DIR
 
 echo "Restarting crosswalk and button service"
 ssh_sudo systemctl restart button
